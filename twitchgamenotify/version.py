1d3fd737dcc739d2270e940e4f38ab181ff45fb2
Matt Wiens <mwiens91@gmail.com>
2018-07-29 20:47:42 -0700

    Bump version

--- twitchgamenotify/version.py
+++ twitchgamenotify/version.py
@@ -1,7 +1,7 @@
 """Contains name, version, description."""
 
 NAME = 'twitch-game-notify'
-VERSION = '0.1.1'
+VERSION = '0.1.2'
 DESCRIPTION = (
     "get notified when your favourite Twitch streamers "
     "play your favourite games")
