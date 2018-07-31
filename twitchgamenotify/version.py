902d0180536d382dfc46fab09c7f6968d4d9cf63
Matt Wiens <mwiens91@gmail.com>
2018-07-30 19:40:04 -0700

    Update recommended query period in example config
    It can be much lower now that we're using JWTs. Also bump the version.

--- twitchgamenotify/version.py
+++ twitchgamenotify/version.py
@@ -1,7 +1,7 @@
 """Contains name, version, description."""
 
 NAME = 'twitch-game-notify'
-VERSION = '0.1.2'
+VERSION = '0.2.0'
 DESCRIPTION = (
     "get notified when your favourite Twitch streamers "
     "play your favourite games")
