9e54e6023570bcc4d236de78418c5f16179c365c
Matt Wiens <mwiens91@gmail.com>
2021-02-24 20:08:51 -0500

    Don't require AppIndicator3 for the tray icon

--- twitchgamenotify/app_indicator.py
+++ twitchgamenotify/app_indicator.py
@@ -1,7 +1,7 @@
 """Contains an application indicator.
 
-Thanks to Timur Rubeko with his wonderful guide here:
-https://candidtim.github.io/appindicator/2014/09/13/ubuntu-appindicator-step-by-step.html.
+I've only tested this on Ubuntu + i3. This should work wherever GTK
+works, though.
 """
 
 import _thread  # pylint: disable=wrong-import-order
@@ -13,12 +13,13 @@ from twitchgamenotify.version import NAME
 # Make sure our GTK stuff is good before loading the app indicator
 try:
     gi.require_version("Gtk", "3.0")
-    gi.require_version("AppIndicator3", "0.1")
 except ValueError as e:
     logging.error(e)
     _thread.interrupt_main()
 
-from gi.repository import Gtk, AppIndicator3 # pylint: disable=wrong-import-order,wrong-import-position,ungrouped-imports
+# fmt: off
+from gi.repository import Gtk # pylint: disable=wrong-import-order,wrong-import-position
+# fmt: on
 
 
 class AppIndicator:
@@ -26,26 +27,26 @@ class AppIndicator:
 
     def __init__(self):
         """Setup the app indicator."""
-        # Setup
-        self.indicator = AppIndicator3.Indicator.new(
-            NAME,
-            APP_INDICATOR_SVG_PATH,
-            AppIndicator3.IndicatorCategory.SYSTEM_SERVICES,
-        )
-        self.indicator.set_status(AppIndicator3.IndicatorStatus.ACTIVE)
-
         # Build the menu
-        menu = Gtk.Menu()
+        self.menu = Gtk.Menu()
         item_quit = Gtk.MenuItem("Quit")
 
         # Wrap the kill_main function
         quit_wrapper = lambda source: _thread.interrupt_main()
 
         item_quit.connect("activate", quit_wrapper)
-        menu.append(item_quit)
-        menu.show_all()
+        self.menu.append(item_quit)
+        self.menu.show_all()
 
-        self.indicator.set_menu(menu)
+        self.indicator = Gtk.StatusIcon(title=NAME, tooltip_text=NAME)
+        self.indicator.set_from_file(APP_INDICATOR_SVG_PATH)
+        self.indicator.connect("popup-menu", self.on_popup_menu)
+
+    def on_popup_menu(self, icon, button, time):
+        """Calls the menu popup."""
+        self.menu.popup(
+            None, None, Gtk.StatusIcon.position_menu, icon, button, time
+        )
 
     @staticmethod
     def start():
