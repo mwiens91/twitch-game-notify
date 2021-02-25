ca095aa49514dd12daa6d71476a3ba3a6c23a880
Matt Wiens <mwiens91@gmail.com>
2021-02-24 20:44:29 -0500

    Bump version to new minor version
    The changes are/are going to be significant and potentially breaking.

--- twitchgamenotify/version.py
+++ twitchgamenotify/version.py
@@ -1,7 +1,7 @@
 """Contains name, version, description."""
 
 NAME = "twitch-game-notify"
-VERSION = "0.2.15"
+VERSION = "0.3.0"
 DESCRIPTION = (
     "get notified when your favourite Twitch streamers "
     "play your favourite games"
