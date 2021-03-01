cb6cf9236bd1bec370ac5943e8260c70018f5591
Matt Wiens <mwiens91@gmail.com>
2021-03-01 12:23:07 -0500

    Bump version (increment patch)

--- twitchgamenotify/version.py
+++ twitchgamenotify/version.py
@@ -1,7 +1,7 @@
 """Contains name, version, description."""
 
 NAME = "twitch-game-notify"
-VERSION = "0.3.0"
+VERSION = "0.3.1"
 DESCRIPTION = (
     "get notified when your favourite Twitch streamers "
     "play your favourite games"
